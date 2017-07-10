8706636509aa9b57f77949c17476e1b29841cbf9
goldface <kjhdoc@naver.com>
2017-07-10 20:54:22 +0900

    [추가] 프로젝트 기본 설정 추가

---
+++ RhythmPong/Assets/Scripts/TestScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Test Script
+/// </summary>
+public class TestScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("TestScript Start!");
+    }
+
+}
